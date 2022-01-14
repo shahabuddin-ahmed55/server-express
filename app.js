074da4abe77dbd2dbd177454088e9d04a75a8f93
Shahab Uddin Ahmed <shahabuddin2401026@gmail.com>
2022-01-14 16:02:33 +0500

    update

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express')
+const bodyParser = require("body-parser")
+const path = require("path")
 const app = express();
 const routes = require("./routes/users")
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs")
+app.set("views", "views")
 
 app.use((req, res, next) => {
   console.log(req.url);
   next();
-})
+});
 
 
 
 app.use("/form", routes)
+
+app.use("/", (req, res) => {
+  res.render("home", {user: "Shahid Chohan"});
+});
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,15 @@
 const express = require("express")
-const { send } = require("express/lib/response")
+const path = require("path")
 const routes = express.Router()
 
-routes.get("/", (req, res, next) => {
-    // res.send(req.url + "Changes");
-    res.send(`
-    <form action="/form" method="POST">
-    <input name="input" placeholder="Favicon" require="require""/>
-    <button>Submit</ button>
-    </form>
-    `);
-    next();
-  })
 
-routes.post("/form", (req, res, next) => {
-    res.send("Shahabuddin");
+routes.post("/", (req, res, next) => {
+    res.send(req.body);
     next();
+})
+routes.get("/", (req, res, next) => {
+// res.send(req.url + "Changes");
+    res.sendFile(path.join(__dirname, "../", "views" , "index.html"));
   })
 
 module.exports = routes
